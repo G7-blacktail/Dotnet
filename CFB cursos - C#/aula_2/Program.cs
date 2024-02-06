984c25ee29d3c6d53788c75f76b8576e1c7c9da2
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-02-06 17:57:20 -0300

    Aprendendo C# para usar o Dotnet

---
+++ CFB cursos - C#/aula_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula_02
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            if(args.GetLength(0) > 0) {
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
