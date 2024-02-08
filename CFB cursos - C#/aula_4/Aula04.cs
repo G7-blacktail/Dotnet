ba8f576abe3cfdf0ad9a109ac697caf1793fddba
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-02-08 14:49:04 -0300

    ""

---
+++ CFB cursos - C#/aula_4/Aula04.cs
@@ -0,0 +1,14 @@
+using System;
+
+    class Aula04
+    {
+        static void Main()
+        {
+            int num = 0;
+        }
+
+        void Test()
+        {
+            Console.Write(num);
+        }
+    }
