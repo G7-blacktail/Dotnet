0a2a9849bcc0fc3a8e58b2b9ec08db383a542aa3
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-09-27 15:25:12 -0300

    Finalizado aula 21 (Do While)

--- CFB cursos - C#/aula_21/Aula21.cs
+++ CFB cursos - C#/aula_21/Aula21.cs
@@ -2,7 +2,26 @@ using System;
 
 class Aula21 {
     static void Main() {
+        //DO While (faça quando)
+        // int num = 5;
+        // do {
+        //     Console.WriteLine("Fazer a base é necessário para avançar com concistência");
+        // } while (num < 5);
+        // Exemplo básico finalizado aqui
 
+        string senha = "123";
+        string senhauser;
+        int tentativas = 0;
+
+        do {
+            Console.Clear();
+            Console.WriteLine("Digite a senha: ");
+            senhauser = Console.ReadLine();
+            tentativas++;
+        } while(senha != senhauser);
+
+        Console.WriteLine("Senha Correta!");
+        Console.WriteLine("Número de tentativas: {0}", tentativas);
 
     }
 }
