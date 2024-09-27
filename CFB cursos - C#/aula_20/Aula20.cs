d4fcf71134e35524acfadb278468733093264121
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-09-27 15:16:30 -0300

    Finalizado aula 20 (While decrementando)

--- CFB cursos - C#/aula_20/Aula20.cs
+++ CFB cursos - C#/aula_20/Aula20.cs
@@ -4,12 +4,12 @@ class Aula20 {
     static void Main() {
         // LOOP While (enquanto)
         int[] num = new int[10];
-        int i = 0;
+        int i = num.Length -1;
 
-        while(i < num.Length) {
+        while(i > 0) {
             num[i] = 0;
             Console.WriteLine(num[i]);
-            i++;
+            i--;
         }
 
         Console.WriteLine("Fim do Loop");
