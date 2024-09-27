a20ebecf1a3b2cd53886578d6bab1e23f4eafa09
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-09-27 11:13:13 -0300

    finalizando aula 7 (constantes)

---
+++ CFB cursos - C#/aula_7/Aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula07 {
+    static void Main() {
+        // Constantes
+        const string canal = "CFB cursos";
+        const double pi=3.1415;
+
+        Console.WriteLine("Canal: {0}\nPI: {1}", canal, pi);
+        Console.WriteLine();
+    }
+}
