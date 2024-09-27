adc4a12ce1408a82b5db08b926f2626750c9e4fe
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-09-27 10:50:50 -0300

    aula de Operadores e Operações

---
+++ CFB cursos - C#/aula_5/Aula05.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Aula05 {
+
+    static void Main() {
+
+        // int res=(10+5)*2;
+        // Console.WriteLine(res);
+
+        // bool res = 10 ! 5;
+        // Console.WriteLine(res);
+
+        int num = 10;
+        num++;
+        num+=1;
+        num = num + 1;
+        Console.WriteLine(num);
+
+        
+    }
+}
